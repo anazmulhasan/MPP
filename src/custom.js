6e78b29bcd7c6f53d6165fa78ea3e0fece60155a
anazmulhasan <nhkhan6965@gmail.com>
2025-09-21 14:19:11 +0600

    new

--- src/custom.js
+++ src/custom.js
@@ -1,3 +1,54 @@
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+const sections = document.querySelectorAll("section");
+
+const activePage = () => {
+  const header = document.querySelector("header");
+  const barsBox = document.querySelector(".bars-box");
+
+  header.classList.add("active");
+  setTimeout(() => {
+    header.classList.remove("active");
+  }, 1100);
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+  barsBox.classList.add("active");
+  setTimeout(() => {
+    barsBox.classList.remove("active");
+  }, 1100);
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+};
+
+navLinks.forEach((link, idx) => {
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
+      activePage();
+
+      link.classList.add("active");
+
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 1100);
+    }
+  });
+});
+
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
+
+    navLinks[0].classList.add("active");
+
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 1100);
+  }
+});
+
 const resumeBtns = document.querySelectorAll(".resume-btn");
 
 resumeBtns.forEach((btn, idx) => {
